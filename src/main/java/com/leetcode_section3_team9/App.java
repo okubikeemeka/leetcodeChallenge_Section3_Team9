e84f802af7e4c73e617ce0de44ddd27e8efe0d53
okubikeemeka <okubikeemeka@gmail.com>
2024-11-27 20:50:42 -0500

    Initial commit

---
+++ src/main/java/com/leetcode_section3_team9/App.java
@@ -0,0 +1,13 @@
+package com.leetcode_section3_team9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
