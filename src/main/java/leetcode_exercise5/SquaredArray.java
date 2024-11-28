b3adb49b02bfb2ccc02233f3a45a18867235b787
Grace Ikechi <mgbechigrace08@gmail.com>
2024-11-27 22:41:39 -0500

    Squares of a Sorted Array

---
+++ src/main/java/leetcode_exercise5/SquaredArray.java
@@ -0,0 +1,38 @@
+package leetcode_exercise5;
+
+import java.util.Arrays;
+
+class SquaredArray {
+	public static int[] sortedSquares(int[] nums) {
+
+		// creating a new array to store the result of the sorted squares
+		int[] sortedNums = new int[nums.length];
+		int index = 0;
+
+		// iterating over the nums array
+		for (int value : nums) {
+
+			// finding the squares of each value in the nums array
+			int squaredNums = value * value;
+
+			// returning the squares in the new array sortedNums created
+			sortedNums[index] = squaredNums;
+			index++;
+		}
+		// array after finding squares of values in it
+		System.out.println("Before sorting: " + Arrays.toString(sortedNums));
+
+		// sorting the squaredNums[]
+		Arrays.sort(sortedNums);
+		return sortedNums;
+	}
+
+	public static void main(String[] args) {
+		int[] sortedNums = sortedSquares(new int[] { -4, -1, 0, 3, 10 });
+
+		// array after sorting squares of values in it
+		System.out.println("After sorting: " + Arrays.toString(sortedNums));
+
+	}
+}
+
